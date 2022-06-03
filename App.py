5a51d387238ab15bba8427fcd44b5b5cd87e8496
AlpYksl <56409177+AlpYksl@users.noreply.github.com>
2022-06-03 14:24:14 +0300

    V2

---
+++ App.py
@@ -0,0 +1,37 @@
+#from flask import Flask
+#app = Flask(__name__)
+
+#@app.route('/')
+#def hello_world():
+   #return "Hello World"
+
+#@app.route("/hello")
+#def hello_world2():
+   #return "hello world"
+
+#@app.route('/hello/<name>')
+#def hello_name(name):
+   #return 'Hello %s!' % name
+
+#if __name__ == '__main__':
+   #app.run()
+   
+   
+from flask import Flask, redirect, url_for, request
+app = Flask(__name__)
+
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
+
+if __name__ == '__main__':
+   app.run(debug = True)
---
+++ KNN.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neighbors import NearestNeighbors
+from scipy.sparse import csr_matrix
+
+books = pd.read_csv('BX-Books.csv', sep=';', error_bad_lines=False, encoding="latin-1")
+books.columns = ['ISBN', 'bookTitle', 'bookAuthor', 'yearOfPublication', 'publisher', 'imageUrlS', 'imageUrlM', 'imageUrlL']
+users = pd.read_csv('BX-Users.csv', sep=';', error_bad_lines=False, encoding="latin-1")
+users.columns = ['userID', 'Location', 'Age']
+ratings = pd.read_csv('BX-Book-Ratings.csv', sep=';', error_bad_lines=False, encoding="latin-1")
+ratings.columns = ['userID', 'ISBN', 'bookRating']
+
+
+combine_book_rating = pd.merge(ratings, books, on='ISBN')
+columns = ['yearOfPublication', 'publisher', 'bookAuthor', 'imageUrlS', 'imageUrlM', 'imageUrlL']
+combine_book_rating = combine_book_rating.drop(columns, axis=1)
+#print(combine_book_rating.head())
+
+combine_book_rating = combine_book_rating.dropna(axis = 0, subset = ['bookTitle'])
+
+book_ratingCount = (combine_book_rating.
+     groupby(by = ['bookTitle'])['bookRating'].
+     count().
+     reset_index().
+     rename(columns = {'bookRating': 'totalRatingCount'})
+     [['bookTitle', 'totalRatingCount']]
+    )
+#print(book_ratingCount.head())
+
+rating_with_totalRatingCount = combine_book_rating.merge(book_ratingCount, left_on = 'bookTitle', right_on = 'bookTitle', how = 'left')
+#print(rating_with_totalRatingCount.head())
+
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+#print(book_ratingCount['totalRatingCount'].describe())
+
+popularity_threshold = 50
+rating_popular_book = rating_with_totalRatingCount.query('totalRatingCount >= @popularity_threshold')
+
+combined = rating_popular_book.merge(users, left_on = 'userID', right_on = 'userID', how = 'left')
+
+us_canada_user_rating = combined[combined['Location'].str.contains("usa|canada")]
+us_canada_user_rating=us_canada_user_rating.drop('Age', axis=1)
+#print(us_canada_user_rating.head())
+
+us_canada_user_rating = us_canada_user_rating.drop_duplicates(['userID', 'bookTitle'])
+us_canada_user_rating_pivot = us_canada_user_rating.pivot(index = 'bookTitle', columns = 'userID', values = 'bookRating').fillna(0)
+us_canada_user_rating_matrix = csr_matrix(us_canada_user_rating_pivot.values)
+
+model_knn = NearestNeighbors(metric = 'cosine', algorithm = 'brute')
+model_knn.fit(us_canada_user_rating_matrix)
+
+query_index = np.random.choice(us_canada_user_rating_pivot.shape[0])
+distances, indices = model_knn.kneighbors(us_canada_user_rating_pivot.iloc[query_index, :].values.reshape(1, -1), n_neighbors = 6)
+
+for i in range(0, len(distances.flatten())):
+    if i == 0:
+        print('Recommendations for {0}:\n'.format(us_canada_user_rating_pivot.index[query_index]))
+    else:
+        print('{0}: {1}, with distance of {2}:'.format(i, us_canada_user_rating_pivot.index[indices.flatten()[i]], distances.flatten()[i]))
+
