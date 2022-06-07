7e68b682b7601533881557db997471726037a598
bengisuaydemir <aydemirbengisu12@gmail.com>
2022-06-07 23:06:13 +0300

    null check

--- App.py
+++ App.py
@@ -17,8 +17,8 @@ def index():
                            book_name = list(popularity_df['Book-Title'].values),
                            author=list(popularity_df['Book-Author'].values),
                            image=list(popularity_df['Image-URL-M'].values),
-                          votes=list(popularity_df['number_ratings'].values),
-                           rating=list(popularity_df['average_rating'].values)
+                           votes=list(popularity_df['number_ratings'].values),
+                           rating=list(np.round(popularity_df['average_rating'].values,2))
                            )
 
 @app.route('/predict')
@@ -27,23 +27,21 @@ def recommend_ui():
 
 @app.route('/predict_books',methods=['post'])
 def recommend():
-    user_input = request.form.get('user_input')
-    index = np.where(final.index == user_input)[0][0]
-    similar_items = sorted(list(enumerate(similar_scores[index])), key=lambda x: x[1], reverse=True)[1:10]
-
     data = []
-    for i in similar_items:
-        item = []
-        temp_df = books[books['Book-Title'] == final.index[i[0]]]
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+    user_input = request.form.get('user_input')
+    array = np.where(final.index == user_input)
+    print(array)
+    if array[0] != None:
+        similar_items = sorted(list(enumerate(similar_scores[array[0][0]])), key=lambda x: x[1], reverse=True)[1:10]
+        for i in similar_items:
+            item = []
+            temp_df = books[books['Book-Title'] == final.index[i[0]]]
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+            data.append(item)
 
-        data.append(item)
-
-    print(data)
-
-    return render_template('predict.html',data=data)
+    return render_template('predict.html',books=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
